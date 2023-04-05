cd00d666f44dced9e83353da34e8cf451c6ad389
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2023-04-06 01:01:18 +0300

    fix gate

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import App from 'components/App';
+import App from './components/App/index';
 import { Provider } from 'react-redux';
 
 import { store } from './Redux/store';
