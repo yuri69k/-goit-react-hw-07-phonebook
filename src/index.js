4838abde53ec1f6d4a1200627e5010fc92d9b234
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2023-04-06 21:22:47 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'modern-normalize/modern-normalize.css';
-import './index.css';
-import App from './components/App/index';
 import { Provider } from 'react-redux';
-
 import { store } from './Redux/store';
+import App from './components/App/index';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
